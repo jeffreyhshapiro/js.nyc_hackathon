3caf696355cfafd3642acd10ea0421521221eff4
Jeffrey Shapiro <jeffreyhshapiro@gmail.com>
2017-01-08 18:34:57 -0500

    fix social media icon formatting on footer

--- src/Footer.js
+++ src/Footer.js
@@ -15,7 +15,7 @@ export default class Footer extends Component {
                   © Copyright 2017 JavaScript NYC
               </Row>
             </Col>
-            <Col md={4}>
+            <Col md={4} sm={4}>
               <Row>
                 <a className="contact-link" target="_blank" href="https://www.meetup.com/JavaScript-New-York-City/">JavaScript.NYC on Meetup</a>
               </Row>
@@ -30,10 +30,10 @@ export default class Footer extends Component {
               </Row>
             </Col>
           </Col>
-          <Col md={2} mdOffset={2}>
-            <Col md={3}><a className="social" target="_blank" href="https://twitter.com/jsnyc2017"><i className="fa fa-twitter" aria-hidden="true"></i></a></Col>
-            <Col md={3}><a className="social" target="_blank" href="https://www.meetup.com/JavaScript-New-York-City/"><i className="fa fa-meetup" aria-hidden="true"></i></a></Col>
-            <Col md={3}><a className="social" target="_blank" href="https://www.facebook.com/JavaScriptNYC/"><i className="fa fa-facebook" aria-hidden="true"></i></a></Col>
+          <Col md={4} sm={4} xs={4}>
+            <Col md={3} sm={3}><a className="social" target="_blank" href="https://twitter.com/jsnyc2017"><i className="fa fa-twitter" aria-hidden="true"></i></a></Col>
+            <Col md={3} sm={3}><a className="social" target="_blank" href="https://www.meetup.com/JavaScript-New-York-City/"><i className="fa fa-meetup" aria-hidden="true"></i></a></Col>
+            <Col md={3} sm={3}><a className="social" target="_blank" href="https://www.facebook.com/JavaScriptNYC/"><i className="fa fa-facebook" aria-hidden="true"></i></a></Col>
           </Col>
         </Row>
       </Grid>
