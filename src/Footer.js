8e372e422b00b168dd27dbccc63de68cee8e3a86
long <lphan1989@gmail.com>
2017-01-08 05:12:12 -0500

    found fb link

--- src/Footer.js
+++ src/Footer.js
@@ -33,7 +33,7 @@ export default class Footer extends Component {
           <Col md={2} mdOffset={2}>
             <Col md={3}><a className="social" href="https://twitter.com/jsnyc2017"><i className="fa fa-twitter" aria-hidden="true"></i></a></Col>
             <Col md={3}><a className="social" href="https://www.meetup.com/JavaScript-New-York-City/"><i className="fa fa-meetup" aria-hidden="true"></i></a></Col>
-            <Col md={3}><a className="social" href="www.facebook.com"><i className="fa fa-facebook" aria-hidden="true"></i></a></Col>
+            <Col md={3}><a className="social" href="https://www.facebook.com/JavaScriptNYC/"><i className="fa fa-facebook" aria-hidden="true"></i></a></Col>
           </Col>
         </Row>
       </Grid>
